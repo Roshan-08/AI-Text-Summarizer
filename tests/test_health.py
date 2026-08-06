2c5580262046fc4debedbfe807e9f028e570d292
Roshan-08 <rkmahto0801@gmail.com>
2026-08-07 00:20:28 +0530

    test: add pytest configuration and initial API test

---
+++ tests/test_health.py
@@ -0,0 +1,8 @@
+def test_home(client):
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    assert response.json() == {
+        "message": "Hello, Roshan! My first FastAPI app is running."
+    }
